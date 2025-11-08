e2fd512959033517538c028e53600ae49eddd7c2
Y108 <57135720+Y108@users.noreply.github.com>
2025-11-08 04:16:21 +0100

    Implement determinant calculation for 3x3 matrix

---
+++ matrix3x3_det.asm
@@ -0,0 +1,74 @@
+.data
+
+Matrix3x3 STRUCT
+
+matrix qword 9 dup(?)
+
+Matrix3x3 ENDS
+
+myMatrix Matrix3x3<{1,2,3,4,5,6,7,8,9}>
+
+.code
+
+det proc ;int64_t det(int64_t* matrixArray)
+
+	xor rax,rax
+
+	mov rdx, 1
+
+	imul rdx, [rcx]
+	imul rdx, [rcx+32]
+	imul rdx, [rcx+64]
+
+	add rax, rdx
+	mov rdx, 1
+
+	imul rdx, [rcx+8]
+	imul rdx, [rcx+40]
+	imul rdx, [rcx+48]
+
+	add rax, rdx
+	mov rdx, 1
+
+	imul rdx, [rcx+16]
+	imul rdx, [rcx+24]
+	imul rdx, [rcx+56]
+
+	add rax, rdx
+	mov rdx, 1
+
+	imul rdx, [rcx+48]
+	imul rdx, [rcx+32]
+	imul rdx, [rcx+16]
+
+	sub rax, rdx
+	mov rdx, 1
+
+	imul rdx, [rcx+56]
+	imul rdx, [rcx+40]
+	imul rdx, [rcx]
+
+	sub rax, rdx
+	mov rdx, 1
+
+	imul rdx, [rcx+64]
+	imul rdx, [rcx+24]
+	imul rdx, [rcx+8]
+
+	sub rax, rdx
+
+	ret
+
+det endp
+
+main proc
+	
+	lea rcx, myMatrix.matrix
+
+	call det
+
+	ret
+
+main endp
+
+end
