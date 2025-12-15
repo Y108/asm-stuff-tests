b52e0bd561793c7c996e4fcad5505f12c9be1fac
Y108 <57135720+Y108@users.noreply.github.com>
2025-12-15 11:03:38 +0100

    added more examples

--- MASM/Modulo/example.c
+++ MASM/Modulo/example.c
@@ -4,8 +4,11 @@
 extern "C" int32_t Modulo(int32_t dividend, int32_t divisor);
 
 int main() {
-
-	printf("%d\n", Modulo(-3,4));
+	
+	printf("13 mod 7  =  %d\n", Modulo(13, 7));
+	printf("-3 mod 4  =  %d\n", Modulo(-3,4));
+	printf("32 mod -5 = %d\n", Modulo(32, -5));
+	printf("-8 mod -3 = %d\n", Modulo(-8, -3));
 
 	return 0;
 }
