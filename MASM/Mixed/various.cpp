2f51888185ab060fd0954507849ff91f6ab32ab9
Y108 <57135720+Y108@users.noreply.github.com>
2026-03-09 04:34:07 +0100

    Create various.cpp with math function declarations
    Add various mathematical function declarations and main function.

---
+++ MASM/Mixed/various.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdint>
+
+extern "C" double _COS(double value);
+extern "C" uint64_t _FACT(uint64_t value);
+extern "C" int64_t _POW(int64_t base, uint64_t exponent);
+extern "C" uint64_t _DOUBLEFACT(uint64_t value);
+extern "C" uint64_t _nCr(uint64_t n, uint64_t r);
+extern "C" double _DIV(uint64_t);
+extern "C" double _POWSD(double base, uint64_t exponent);
+
+// Cosine is not done yet!!
+
+int main(){
+	std::cout << _POWSD(2.5, 5) << '\n';
+	return 0;
+}
