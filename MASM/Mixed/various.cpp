f0c37691d297ae398ee66d3e777ff3890e55923f
Y108 <57135720+Y108@users.noreply.github.com>
2026-03-26 11:06:17 +0100

    Update various.cpp

--- MASM/Mixed/various.cpp
+++ MASM/Mixed/various.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdint>
 
+
 extern "C" double _COS(double value);
 extern "C" uint64_t _FACT(uint64_t value);
 extern "C" int64_t _POW(int64_t base, uint64_t exponent);
@@ -8,10 +9,9 @@ extern "C" uint64_t _DOUBLEFACT(uint64_t value);
 extern "C" uint64_t _nCr(uint64_t n, uint64_t r);
 extern "C" double _DIV(uint64_t);
 extern "C" double _POWSD(double base, uint64_t exponent);
-
-// Cosine is not done yet!!
+extern "C" uint64_t _SUM(uint64_t value);
 
 int main(){
-	std::cout << _POWSD(2.5, 5) << '\n';
+	std::cout << _nCr(12,4);
 	return 0;
 }
